856a1552d2f42da7d3a3b934307227848f67f7c1
JayR1213 <111758139+JayR1213@users.noreply.github.com>
2023-10-28 17:20:21 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -38,3 +38,8 @@ class Var(object):
     DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://jayeshdb:bQmBrMvsaOSGb5dZ@cluster0.moiu8ck.mongodb.net/?retryWrites=true&w=majority"))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+
+    STREAM_URL = (environ.get('STREAM_URL', "True") == "True")
+    STREAM_API = environ.get("STREAM_API", "8e30cbc70ca83e674f9ce4e8a21d084f855d616c") 
+    STREAM_SITE = environ.get("STREAM_SITE", "paisakamalo.in") 
+    STREAM_LONG = environ.get("STREAM_LONG", False)
